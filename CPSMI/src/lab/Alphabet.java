a16f03f5f876ca7f1950e0bcdb187058d8ade3e1
v-bobko <80197626+v-bobko@users.noreply.github.com>
2021-03-28 21:28:42 +0300

    Добавлено 5 лаб

---
+++ CPSMI/src/lab/Alphabet.java
@@ -0,0 +1,10 @@
+package lab;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+
+        }
+    }
+}
---
+++ CPSMI/src/lab/HelloWord.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x <= 100) {
+            System.out.println("Hello Word! " + x);
+            x++;
+        }
+    }
+}
