6902e045b8270f1334fa79db4ff38a61beef4ab0
v-bobko <bobko356@ya.ru>
2021-04-28 20:03:59 +0300

    добавлена 13 лаба, корр-я 9-ой

---
+++ CPSMI/src/lab_13/SledMatrici.java
@@ -0,0 +1,27 @@
+package lab_13;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class SledMatrici {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер матрицы");
+        int a = in.nextInt();
+        int arr[][] = new int[a][a];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                Random r = new Random();
+                arr[i][j] = r.nextInt(100);
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int sled = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sled += arr[i][i];
+        }
+        System.out.println("СЛЕД МАТРИЦЫ =" + sled);
+    }
+}
